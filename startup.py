bf8ba0f22e993150e5df6c9c16f48c1ab520167b
naresh <naresh.v@imaginea.com>
2021-01-15 20:14:21 +0530

    1) change - change startup script to make use of configs

--- startup.py
+++ startup.py
@@ -1,1 +1,10 @@
+# 3rd party modules
+import uvicorn
+
+# applicaion modules
 from src.application import app
+from src.config import config
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=config.host, port=config.port, debug=True, log_level="trace") 
