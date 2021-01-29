a259127d5ecd2b3c5a7128d7f99730562eb39588
naresh <naresh.v@imaginea.com>
2021-01-29 12:43:46 +0530

    Started serving custom swagger static files for below changes1) collapse routes in swagger UI 2) hide schemas

--- src/application.py
+++ src/application.py
@@ -1,6 +1,12 @@
 # 3rd party modules
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 
 # application modules
 from src.routes import label, collection, speech_api
@@ -8,7 +14,8 @@ from src.utils import exception_handler
 
 
 # create FastAPI instance
-app = FastAPI(title="VoiceGen Admin",  description="A REST API service to be used as voicegen admin backend", exception_handlers={Exception: exception_handler, HTTPException: exception_handler})
+app = FastAPI(title="VoiceGen Admin",  description="A REST API service to be used as voicegen admin backend", exception_handlers={Exception: exception_handler, HTTPException: exception_handler}, docs_url=None, redoc_url=None)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # add routes
 app.include_router(label.router)
@@ -25,3 +32,20 @@ app.add_middleware(
 @app.get("/", include_in_schema=False)
 async def root():
     return {"message": "Hello, I'm voicegen admin API!!"}
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css"
+    )
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
